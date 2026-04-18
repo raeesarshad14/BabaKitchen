5030b5bc3b58e4d63d02204bab8045c6540e2c50
Raees Arshad <Raees.Arshad@tibercreek.com>
2026-04-18 16:15:13 -0400

    Initial frontend setup with header and assets

---
+++ frontend/components/header.js
@@ -0,0 +1,25 @@
+class Header {
+  render() {
+    return `
+            <header class="header">
+                <div class="header-left">
+                    <div class="logo">
+                          <img src="../assets/logo picture.png" alt="Logo">
+                    </div>
+
+                    <nav class="nav-links">
+                        <a href="menu.html">MENU</a>
+                        <a href="catering.html">CATERING</a>
+                        <a href="babakitchen.html">BABAKITCHEN</a>
+                        <a href="help.html">HELP</a>
+                    </nav>
+                </div>
+
+                <div class="header-right">
+                    <a class="signin" href="signin.html">SIGN IN</a>
+                    <a class="order-btn" href="order.html">ORDER NOW</a>
+                </div>
+            </header>
+        `;
+  }
+}
