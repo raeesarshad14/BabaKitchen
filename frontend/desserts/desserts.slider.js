7ef9ca52c51f80ef4c509193cfe3d44c46934de0
Raees Arshad <Raees.Arshad@tibercreek.com>
2026-05-08 21:42:54 -0400

    updated pictures and added clickcable insta link on dert first slide

--- frontend/desserts/desserts.slider.js
+++ frontend/desserts/desserts.slider.js
@@ -1,6 +1,7 @@
 let dessertSlideIndex = 0;
 
 const dessertSlides = [
+  "./assets/JanSisters.jpeg",
   "./assets/dessert1.jpeg",
   "./assets/dessert2.jpg",
   "./assets/dessert3.jpg",
@@ -18,7 +19,28 @@ function loadDessertSlider() {
   const slider = document.getElementById("dessertSlider");
 
   slider.innerHTML = dessertSlides
-    .map((src) => `<img class="dessert-slide-img" src="${src}">`)
+    .map((src, index) => {
+      if (index === 0) {
+        return `
+    <a href="https://www.instagram.com/jansistersbakery?igsh=MTZ2Ym91M2xxM2llbg%3D%3D&utm_source=qr"
+       target="_blank"
+       class="dessert-slide-link">
+      <img class="dessert-slide-img" src="${src}">
+
+      <div class="insta-center-box">
+        <div class="insta-subtext">Click below to visit</div>
+
+        <div class="insta-floating">
+          <img src="./assets/instagram.png" alt="Instagram">
+          Visit Jan Sisters Bakery
+        </div>
+      </div>
+    </a>
+  `;
+      }
+
+      return `<img class="dessert-slide-img" src="${src}">`;
+    })
     .join("");
 
   updateDessertSlider();
